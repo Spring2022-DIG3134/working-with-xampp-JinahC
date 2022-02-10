73c5125756d4f7206f3cc0a60f82bccd4b632962
Fridjinah <jinahcheetah@gmail.com>
2022-02-10 00:35:08 -0500

    echo input
    changed the contents of the index file and created a new file to spit out the input of the form

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-    echo("french fries");
-?>
+<html>
+  <head>
+    <title>Lab 4</title>
+  </head>
+  <body>
+    <form action="lab4.php" method="POST">
+      <input type="text" name="lab4_username"></input>
+      <input type="submit"></input>
+    </form>
+  </body>
+</html>
---
+++ lab4.php
@@ -0,0 +1,3 @@
+<?php
+    echo($_POST["lab4_username"]);
+?>
